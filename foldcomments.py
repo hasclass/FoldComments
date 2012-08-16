328473669422bb04ebc2672a8b169aede88d7561
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-08-16 12:03:43 +0200

    do not fold single line comments. add command toggle_comments

--- foldcomments.py
+++ foldcomments.py
@@ -1,17 +1,34 @@
 import sublime, sublime_plugin
 
-class FoldFileComments(sublime_plugin.EventListener):
-    def on_load(self, view):
-        for region in view.find_by_selector('comment'):
-            view.fold(sublime.Region(region.a, region.b - 1))
+# class FoldFileComments(sublime_plugin.EventListener):
+#     def on_load(self, view):
+#         for region in view.find_by_selector('comment'):
+#             offset = view.find('#', region.a).b
+#             view.fold(sublime.Region(offset, region.b - 1))
+
+class ToggleCommentsCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        view     = self.view
+        comments = view.find_by_selector('comment')
+
+        if view.fold(comments[0]):
+            # first comment is unfolded. fold everything
+            for region in view.find_by_selector('comment'):
+                # do not single line comments
+                if len(view.lines(region)) > 1:
+                  offset = view.find('#', region.a).b
+                  view.fold(sublime.Region(offset, region.b - 1))
+        else:
+            view.unfold(comments)
 
 class FoldCommentsCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        for region in self.view.find_by_selector('comment'):
-            self.view.fold(sublime.Region(region.a, region.b - 1))
-
+        for region in view.find_by_selector('comment'):
+            if len(view.lines(region)) > 1:
+              offset = view.find('#', region.a).b
+              view.fold(sublime.Region(offset, region.b - 1))
 
 class UnfoldCommentsCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        for region in self.view.find_by_selector('comment'):
-            self.view.unfold(sublime.Region(region.a, region.b - 1))
+        view = self.view
+        view.unfold(view.find_by_selector('comment'))
